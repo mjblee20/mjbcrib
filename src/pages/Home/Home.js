c74c63b5fd53c41e79c01cba748840d070b907e1
MonteCristo <mjblee20@gmail.com>
2020-01-07 16:11:03 -0800

    basic homepage

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import KnowledgeCluster from '../../templates/KnowledgeCluster/KnowledgeCluster';
 
 import Jumbotron from 'react-bootstrap/Jumbotron';
+import Button from 'react-bootstrap/Button'
 
 import Styles from './Home.module.css';
 
@@ -10,18 +11,20 @@ class Home extends Component {
     return (
       <div> 
         <Jumbotron>
-          <div className={Styles.titleBox}>
-            <h1 className={Styles.titleHead}>Welcome to MJB_Crib</h1>
-          </div>
-          
-          <p className={Styles.description}>This is a website where I'd be working on my portfolio as well as</p>
+          <h1 className={Styles.titleHead}>Welcome to MJB_Crib</h1>
+          <br />
+          <p className={Styles.description}>This website will be a place where I put all my work and projects in.</p>
+          <br />
           <hr className="my-2" />
-          <p>More info</p>
-          <p className="lead">
-            <a className="btn btn-primary btn-lg" href="/portfolio" role="button">Jump to My portfolio</a>
-          </p>
+          <br />
+          <p>Below is the list of my skillsets:</p>
+          <KnowledgeCluster />
         </Jumbotron>
-        <KnowledgeCluster />
+
+        <div className={Styles.btnRightAlign}> 
+          <Button className={Styles.portfolioBtn} href='/portfolio' variant='primary'>View Portfolio</Button>
+        </div>
+        
       </div>
     )
   }
