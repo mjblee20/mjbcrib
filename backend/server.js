bbeb78d2e2794cdf06f8978b2234fe813c5d9cb6
MonteCristo <mjblee20@gmail.com>
2020-01-07 22:04:17 -0800

    mongodb live fix

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 const app = express();
-const port = process.env.PORT || 5000
+const port = process.env.MONGODB_URI || 5000
 
 // Middleware Functions
 app.use(cors());
