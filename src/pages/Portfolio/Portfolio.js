30d3f742490cf67735bdb9808bde027bb62b7a76
MonteCristo <mjblee20@gmail.com>
2020-01-13 12:59:46 -0800

    project links

--- src/pages/Portfolio/Portfolio.js
+++ src/pages/Portfolio/Portfolio.js
@@ -4,9 +4,10 @@ import React from 'react';
 // import Button from 'react-bootstrap/Button';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
-import ListGroup from 'react-bootstrap/ListGroup'
+import ListGroup from 'react-bootstrap/ListGroup';
+import Card from 'react-bootstrap/Card';
 
-// import Styles from './Portfolio.module.css';
+import Styles from './Portfolio.module.css';
 import AppStyles from './../../App.module.css';
 class Portfolio extends React.Component {
   constructor(props) {
@@ -29,39 +30,67 @@ class Portfolio extends React.Component {
         </Row>
 
         <h4>Exercise Log</h4>
-        <ListGroup horizontal>
+        <ListGroup>
           <ListGroup.Item href="/exercises" action variant='info'>Exercise Log</ListGroup.Item>
           <ListGroup.Item href="/users" action variant='info'>User Log</ListGroup.Item>
         </ListGroup>
 
+        <br /> 
+
         <h4>Productivity</h4>
-        <ListGroup horizontal>
+        <ListGroup>
           <ListGroup.Item href="/todo" action variant='info'>ToDo List</ListGroup.Item>
           <ListGroup.Item href="/calendar" action variant='info'>Calendar</ListGroup.Item>
         </ListGroup>
 
-      {/* get from database and populate the section with up to 3 projects */}
-
-        {/* allow adding to database with new projects */}
-
-        {/* TODO: List of projects in card formats */}
-        {/* TODO: Button for every card to github and the page they are located */}
-        {/* TODO: Maybe scrap github to grab information for the newest projects etc. */}
+        <br /> 
 
         <h4>Projects</h4>
         <Row>
           <Col>
-          
+            <Card style={{ width: '18rem' }}>
+              <Card.Body>
+                <Card.Title>AllThingsJavascript</Card.Title>
+                <Card.Subtitle className="mb-2 text-muted">Solo Project</Card.Subtitle>
+                <Card.Text className={Styles.description}>
+                  A Node.js project of my own where I practice the coding challenges and problems. A little bit of Javascript learning will go here as well. 
+                </Card.Text>
+                <Card.Link href="https://github.com/mjblee20/js-algorithms-and-data-structures">GitHub Link</Card.Link>
+                <Card.Link href="#" disabled>Web Link</Card.Link>
+              </Card.Body>
+            </Card>
           </Col>
 
           <Col>
-
+            <Card style={{ width: '18rem' }}>
+              <Card.Body>
+                <Card.Title>In or Out</Card.Title>
+                <Card.Subtitle className="mb-2 text-muted">Bootcamp Project</Card.Subtitle>
+                <Card.Text className={Styles.description}>
+                  A web application designed to help decide whether to eat in or dine out. Once the choice is selected, three choices will randomly be selected and displayed.
+                </Card.Text>
+                <Card.Link href="https://github.com/mjblee20/js-algorithms-and-data-structures">GitHub Link</Card.Link>
+                <Card.Link href="https://willcoe.github.io/Project1/">Web Link</Card.Link>
+              </Card.Body>
+            </Card>
           </Col>
 
           <Col>
-          
+            <Card style={{ width: '18rem' }}>
+              <Card.Body>
+                <Card.Title>Instapotty</Card.Title>
+                <Card.Subtitle className="mb-2 text-muted">Bootcamp Project</Card.Subtitle>
+                <Card.Text className={Styles.description}>
+                  A web application similar in function as Instagram, but with a TWIST! Here only memorable/breathtaking/mind-blowing bathroom photos will be posted.
+                </Card.Text>
+                <Card.Link href="https://github.com/ivancho277/fancyBathroom">GitHub Link</Card.Link>
+                <Card.Link href="instapotty.herokuapp.com">Web Link</Card.Link>
+              </Card.Body>
+            </Card>
           </Col>
         </Row>
+
+        <br /> 
       </div>
     );
   }
