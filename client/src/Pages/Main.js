0a8af4d4e411539ef043368b287214b10cfe0be2
MonteCristo <mjblee20@gmail.com>
2020-05-03 17:02:28 -0700

    work update

--- client/src/Pages/Main.js
+++ client/src/Pages/Main.js
@@ -6,24 +6,36 @@ import { Link } from 'react-router-dom';
 function Main() {
   return (
     <div id="main-grid">
+
       <div id="main-sect">
         <div id="main-title">
+
           <p>
             Hi,
             <br/> 
-            I'm Bill.
+            I'm <span id="name">Bill.</span> 
             <br/>
+            Junior Web Developer
           </p>
-          <br/>
-          <p id="main-description">I'm a young and passionate Full Stack Web Developer with a background in Biophysics looking to create simple, intuitive, and user-friendly web applications. I am proficient in the MERN Stack and looking to expand my knowledge in the web development field.</p>
+
+          <div id="main-description">
+            <ul className="noStyle">
+              <li>UW Biophysics Grad</li> /
+              <li>Full-Stack Web Developer</li> /
+              <li>Coffee Lover</li>
+            </ul>
+          </div>
+
         </div>
       </div>
+      
       <div id="btn-sect">
         <ul id="btn-sect" className='noStyle'>
           <li><Link to="/portfolio">View My Work</Link></li>
           <li><Link to="/contact">Contact Me</Link></li>
         </ul>
       </div>
+
     </div>
   )
 }
